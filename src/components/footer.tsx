efd9c2eb00c25bfa05f3709b30dfb6fda1d76a98
Bryce Mehring <brycemehring@gmail.com>
2020-03-30 22:42:22 -0500

    Extract fontawesome css from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { config } from '@fortawesome/fontawesome-svg-core'
 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default () => {
   const { site } = useStaticQuery(
@@ -24,13 +28,13 @@ export default () => {
       <ul className="contact-list">
         <li>
           <a href={github}>
-            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faGithub} size="1x" />
             Github
           </a>
         </li>
         <li>
           <a href={`mailto:${email}`}>
-            <FontAwesomeIcon icon={faEnvelope} />
+            <FontAwesomeIcon icon={faEnvelope} size="1x" />
             {email}
           </a>
         </li>
