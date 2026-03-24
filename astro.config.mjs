fef604178a5b25d24e806c3f0690b717ae94112a
Bryce Mehring <brycemehring@gmail.com>
2026-03-23 21:49:14 -0500

    use @fontsource-variable/open-sans instead of inline font

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brycemehring.com',
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: 'Open Sans',
-      cssVariable: '--font-open-sans',
-      weights: [300, 400, 500, 700],
-    },
-  ],
   vite: {
     css: {
       preprocessorOptions: {
